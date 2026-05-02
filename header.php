2df21d09f720a9c03db70dde754f8afad4865b3d
wsmorais <wilson1first@gmail.com>
2026-05-02 00:59:53 -0300

    Mudanças no Logo e Menu

--- header.php
+++ header.php
@@ -1,15 +1,19 @@
 <!-- Barra de navegação -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php"><img src="logo-PI-G2-peq.png" alt="Logotipo"></a>
+    <a class="navbar-brand" href="index.php">
+  <img src="logo-PI-G2.png" alt="Logotipo" width="150" height="auto">
+</a>
     <div class="collapse navbar-collapse">
       <ul class="navbar-nav me-auto">
         <!-- Links principais -->
-        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-        <li class="nav-item"><a class="nav-link" href="servicos.php">Serviços</a></li>
-        <li class="nav-item"><a class="nav-link" href="agendamento.php">Agendamento</a></li>
-        <li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>
+        <li class="nav-item"><a class="nav-link fs-5" href="index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link fs-5" href="servicos.php">Serviços</a></li>
+        <li class="nav-item"><a class="nav-link fs-5" href="agendamento.php">Agendamento</a></li>
+        <li class="nav-item"><a class="nav-link fs-5" href="logout.php">Logout</a></li>
       </ul>
     </div>
   </div>
+
 </nav>
+
--- index.php
+++ index.php
@@ -7,7 +7,9 @@ include 'auth.php';    // funções de login/logout
 <head>
   <meta charset="UTF-8">
   <title>Home - Barbearia</title>
+
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
 </head>
 <body class="bg-light">
 <?php include 'header.php'; // inclui barra de navegação ?>
